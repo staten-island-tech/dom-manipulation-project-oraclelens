01f7dedb6536544ef19db5cd111a43a2ff356204
oraclelens <sophiar10200@gmail.com>
2022-11-02 13:13:22 -0400

    its colangelos birthday

---
+++ hi.js
@@ -0,0 +1,6 @@
+const DOMselectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  point: document.querySelectorAll(".point"),
+};
+console.log(DOMselectors);
