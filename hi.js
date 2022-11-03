215f7da538b7e968fb3c1f86ab902a899ee7247a
oraclelens <sophiar10200@gmail.com>
2022-11-03 13:14:20 -0400

    apwh

--- hi.js
+++ hi.js
@@ -1,6 +1,16 @@
-const DOMselectors = {
+const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
+  box: document.getElementById("box"),
   point: document.querySelectorAll(".point"),
 };
-console.log(DOMselectors);
+
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "pink";
+  text.textContent = "This is pink now";
+  text.style.fontSize = "40px";
+}
+
+DOMSelectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
