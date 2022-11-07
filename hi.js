6cc50da831cc0d382d606dd56db579dac606e05b
oraclelens <sophiar10200@gmail.com>
2022-11-07 13:16:09 -0500

    yeah

--- hi.js
+++ hi.js
@@ -1,8 +1,27 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
+  box: document.getElementById("container-box"),
+  input: document.querySelector(`#input`),
+};
+
+DOMSelectors.button.addEventListener("click", function () {
+  let input = DOMSelectors.input.value;
+  console.log(input);
+  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+  DOMSelectors.input.value = "";
+});
+
+/*
+const cat = "meow";
+
+DOMSelectors.box.insertAdjacentHTML("afterend", `<h1>We are a ${cat}</h1>`);
+*/
+/*
+const DOMSelectors = {
+  button: document.getElementById("btn"),
   text: document.querySelector("#text"),
   box: document.getElementById("box"),
-  point: document.querySelectorAll(".point"),
+  points: document.querySelectorAll(".points"),
 };
 
 function backgroundAndText(background, text) {
@@ -14,3 +33,14 @@ function backgroundAndText(background, text) {
 DOMSelectors.button.addEventListener("click", function () {
   backgroundAndText(DOMSelectors.box, DOMSelectors.text);
 });
+
+function changeLi() {
+  let pointIndex = 1;
+  DOMSelectors.points.forEach((point) => {
+    point.addEventListener("click", function () {
+      point.textcontent = `Hello I am ${pointIndex}`;
+      pointIndex++;
+    });
+  });
+}
+changeLi(); */
