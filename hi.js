4ec583a9574586d05c025e49fb1c5cc966aae2bd
oraclelens <sophiar10200@gmail.com>
2022-11-16 13:15:08 -0500

    delete btn is an issue

--- hi.js
+++ hi.js
@@ -1,7 +1,29 @@
 const DOMSelectors = {
-  title: document.getElementById("title"),
-  artist: document.getElementbyId("artist"),
-  imageurl: document.getElementbyId("url"),
-  makecard: document.getElementById("makecard"),
+  songtitle: document.getElementById("title"),
+  artistname: document.getElementById("artist"),
+  imageurl: document.getElementById("imageurl"),
+  makecard: document.getElementById("create"),
   box: document.getElementById("box"),
+  container: document.getElementById("container"),
+  display: document.querySelector("display"),
+  delete: document.getElementById("delete"),
 };
+
+function addNewDiv() {}
+
+DOMSelectors.makecard.addEventListener("click", function () {
+  let songtitle = DOMSelectors.songtitle.value;
+  let artistname = DOMSelectors.artistname.value;
+  let imageurl = DOMSelectors.imageurl.value;
+
+  DOMSelectors.container.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="display">
+      <image src></image>
+      <h1 class="songtitles">${songtitle}</h1>
+      <h2 class="artists">${artistname}</h2>
+      <img src="${imageurl}" class="myImage"alt="">
+      <button id="delete" onclick="this.parentElement.remove()">Delete</button>
+    </div>`
+  );
+});
